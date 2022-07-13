d929ee2a1b5d329221f40723a8ba754f2e5fc38d
Juan Canelon <liscanojac@gmail.com>
2022-07-13 19:36:04 -0300

    feat: setting pokemon store

--- src/store/pokemon.ts
+++ src/store/pokemon.ts
@@ -1,5 +1,8 @@
+import { Commit } from "vuex"
 import { PokemonState } from "@/interfaces/pokemon-state"
 import { Pokemon } from "@/interfaces/pokemon"
+import { page_limit } from "@/interfaces/pages-state"
+import { apiService } from "@/api-service/pokemon"
 
 const state = {
   pokemon: [] as Array<Pokemon>,
@@ -12,8 +15,50 @@ const getters = {
   getPokemonDetails: (state: PokemonState) => state.pokemonDetails
 }
 
+const actions = {
+  async fetchPokemon({ commit } : { commit: Commit }, page: number) {
+    const pageOffset = (page * page_limit) - page_limit
+    
+    const pokemons = await apiService.getPokemons(pageOffset) as Array<Pokemon>
+
+    commit("setPokemon", pokemons)
+  },
+  async fetchTotalPokemon({ commit } : { commit: Commit }) {
+
+    const totalPokemon = await apiService.getTotalPokemon() as number
+
+    commit("setTotalPokemon", totalPokemon)
+  },
+  async fetchPokemonDetails({ commit } : { commit: Commit }, id: number) {
+
+    const pokemonDetails = await apiService.getPokemonDetails(id) as Pokemon
+
+    commit("setPokemonDetails", pokemonDetails)
+  },
+  clearPokemonDetails({ commit } : { commit: Commit }) {
+    commit("setPokemonDetailsCleared")
+  }
+}
+
+const mutations = {
+  setPokemon: (state: PokemonState, pokemons: Array<Pokemon>) => {
+    state.pokemon = pokemons
+  },
+  setTotalPokemon: (state: PokemonState, totalPokemon: number) => {
+    state.totalPokemon = totalPokemon
+  },
+  setPokemonDetails: (state: PokemonState, pokemon: Pokemon) => {
+    state.pokemonDetails = pokemon
+  },
+  setPokemonDetailsCleared: (state: PokemonState) => {
+    state.pokemonDetails = {} as Pokemon
+  }
+}
+
 export const pokemon = {
   namespaced: true,
   state,
-  getters
+  getters,
+  actions,
+  mutations
 }
