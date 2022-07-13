30d58bc3805941b1c63e3cb55eb12058c4d5c037
Juan Canelon <liscanojac@gmail.com>
2022-07-13 19:50:33 -0300

    feat: adding google fonts

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,9 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css2?family=DotGothic16&family=VT323&display=swap');
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url('assets/pokemon_background.jpg');
 }
 </style>
