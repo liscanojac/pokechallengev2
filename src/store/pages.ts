4ec0c79d14198d33829f98017d46fe5421bf490b
Juan Canelon <liscanojac@gmail.com>
2022-07-13 16:31:34 -0300

    feat: pages vuex module

---
+++ src/store/pages.ts
@@ -0,0 +1,61 @@
+import { Commit } from "vuex"
+import { PagesState, page_limit } from "@/interfaces/pages-state"
+
+const state = {
+  page: 1,
+  totalPages: 0,
+} as PagesState
+
+const getters = {
+  getPage: (state: PagesState) => state.page,
+  getTotalPages: (state: PagesState) => state.totalPages
+}
+
+const actions = {
+  nextPage({ commit } : { commit: Commit }) {
+    commit("setNextPage")
+  },
+  prevPage({ commit } : { commit: Commit }) {
+    commit("setPrevPage")
+  },
+  initialPage({ commit } : { commit: Commit }) {
+    commit("setInitialPage")
+  },
+  lastPage({ commit } : { commit: Commit }) {
+    commit("setLastPage")
+  },
+  totalPages({ commit } : { commit: Commit }, totalPokemon: number) {
+    commit("setTotalPages", totalPokemon)
+  }
+}
+
+const mutations = {
+  setNextPage: (state: PagesState) => {
+    if (state.page < page_limit) {
+      state.page = state.page + 1
+    }
+  },
+  setPrevPage: (state: PagesState) => {
+    if (state.page < 1) {
+      state.page = state.page - 1
+    }
+  },
+  setInitialPage: (state: PagesState) => {
+    state.page = 1
+  },
+  setLastPage: (state: PagesState) => {
+    state.page = state.totalPages
+  },
+  setTotalPages: (state: PagesState, totalPokemon: number) => {
+    state.totalPages = Math.ceil(totalPokemon / page_limit)
+  }
+}
+
+
+export const pages = {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
