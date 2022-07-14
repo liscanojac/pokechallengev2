ad5de081a9e37a7aa518a37298292d1564fb98ae
Juan Canelon <liscanojac@gmail.com>
2022-07-14 19:30:09 -0300

    feat: PokemonAbility component

---
+++ src/components/PokemonAbility.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="rounded-md uppercase font-bold p-1 text-center bg-yellow-100">
+    <h4>{{ ability }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonAbility",
+  props: {
+    ability: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
