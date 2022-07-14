caa82ea3039fe42fd2becf2d76a184c225fd0c84
Juan Canelon <liscanojac@gmail.com>
2022-07-14 19:32:23 -0300

    feat: PokemonType component

---
+++ src/components/PokemonType.vue
@@ -0,0 +1,32 @@
+<template>
+  <div 
+    class="rounded-md uppercase font-bold p-1 text-center"
+    :class="typeClass"
+  >
+    <h4>{{ pokemonTypeLabel }}</h4>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'PokemonType',
+  props: {
+    pokemonType: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters({ language: 'getLanguage' }),
+    ...mapGetters('languageSwitch', ['getTypeNamesTranslation', 'getTypeColors']),
+    pokemonTypeLabel() {
+      return this.getTypeNamesTranslation(this.pokemonType, this.language)
+    },
+    typeClass() {
+      return this.getTypeColors(this.pokemonType)
+    }
+  }
+}
+</script>
