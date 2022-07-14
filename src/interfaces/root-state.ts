ca0df7cb8b7c1a78b0ed609a2c34534d3c3601cc
Juan Canelon <liscanojac@gmail.com>
2022-07-13 22:01:13 -0300

    feat: loading and language store props

---
+++ src/interfaces/root-state.ts
@@ -0,0 +1,5 @@
+
+export interface RootState {
+  loading: boolean
+  language: string
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,28 @@
-import { createStore } from 'vuex'
+import { Commit, createStore } from 'vuex'
 import { pokemon } from './pokemon'
 import { pages } from './pages'
+import { RootState } from '@/interfaces/root-state'
 
 export default createStore({
   state: {
-    loading: false as boolean
+    loading: false as boolean,
+    language: "en" as string
   },
   getters: {
+    getLoading: (state: RootState) => state.loading,
+    getLanguage: (state: RootState) => state.language
   },
   mutations: {
+    changeLanguage: (state: RootState) => state.language === "en" ? state.language = "es" : state.language = "en",
+    changeLoading: (state: RootState) => state.loading = !state.loading
   },
   actions: {
+    switchLanguage({ commit } : { commit: Commit }) {
+      commit("changeLanguage")
+    },
+    switchLoading({ commit } : { commit: Commit }) {
+      commit("changeLoading")
+    }
   },
   modules: {
     pokemon,
