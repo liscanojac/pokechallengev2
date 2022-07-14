2c6a5bdbc5670ad29dd29dfec0d844164be0b926
Juan Canelon <liscanojac@gmail.com>
2022-07-14 19:26:19 -0300

    feat: LanguageToggler component

---
+++ src/components/LanguageToggler.vue
@@ -0,0 +1,46 @@
+<template>
+  <label
+    @click="switchLanguage"
+    for="toggle" 
+    class="flex items-center cursor-pointer relative mb-4"
+  >
+    <span class="mr-3 text-gray-900 text-sm font-medium">EN</span>
+    <input
+      :checked="language === 'es'"
+      type="checkbox" 
+      class="sr-only"
+    >
+    <div class="toggle-bg bg-emerald-200 border-2 border-gray-200 h-6 w-11 rounded-sm"></div>
+    <span class="ml-3 text-gray-900 text-sm font-medium">ESP</span>
+  </label>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'LanguageToggler',
+  computed: {
+    ...mapGetters({ language: 'getLanguage' })
+  },
+  methods: {
+    ...mapActions(['switchLanguage'])
+  }
+}
+</script>
+
+<style scoped>
+.toggle-bg:after {
+  content: '';
+  @apply absolute top-0.5 left-7 bg-orange-300 border border-orange-300 rounded-sm h-5 w-5 transition shadow-sm;
+}
+
+input:checked+.toggle-bg:after {
+  transform: translateX(100%);
+  @apply border-orange-300;
+}
+
+input:checked+.toggle-bg {
+  @apply bg-emerald-600 border-emerald-600;
+}
+</style>
