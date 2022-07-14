f1cb4955af9f73338c7c7a13750300eca0dc1291
Juan Canelon <liscanojac@gmail.com>
2022-07-14 19:40:12 -0300

    refactor: setting Pokedex and classes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="display flex flex-col justify-center items-center mt-12 px-6">
     <PokeDex />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { mapGetters } from 'vuex';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import PokeDex from '@/components/PokeDex.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
     PokeDex
-  },
-  computed: {
-    ...mapGetters({ language: 'getLanguage'}),
-    pokedexMessage() {
-      
-    }
   }
 });
 </script>
