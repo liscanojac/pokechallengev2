56703fe6d283fe0f5864e8cb3c04c6b437959751
Juan Canelon <liscanojac@gmail.com>
2022-07-14 19:33:14 -0300

    feat: DetailView view

---
+++ src/views/DetailView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col justify-center items-center mt-12 px-6">
+    <PokemonDetails />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import PokemonDetails from '@/components/PokemonDetails.vue'
+
+export default {
+  name: "DetailsView",
+  components: {
+    PokemonDetails
+  },
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  },
+  async created() {
+    this.switchLoading()
+    await this.fetchPokemonDetails(this.id)
+    this.switchLoading()
+  },
+  methods: {
+    ...mapActions('pokemon', ['fetchPokemonDetails']),
+    ...mapActions(['switchLoading'])
+  }
+}
+</script>
